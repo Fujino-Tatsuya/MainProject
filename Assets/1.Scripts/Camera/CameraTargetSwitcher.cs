879baec6689ef8bbc922965ca701af9ec3f0e1ba
SilverLight <silverlight0522@gmail.com>
2026-06-04 20:52:58 +0900

    카메라 테스트씬 만드는 겸.... 기본 한글 폰트 에셋 만들어서 넣는 겸.... TMP 패키지 까지 추가 했음...

--- Assets/1.Scripts/Camera/CameraTargetSwitcher.cs
+++ Assets/1.Scripts/Camera/CameraTargetSwitcher.cs
@@ -1,14 +1,12 @@
 using System.Collections.Generic;
 using Unity.Cinemachine;
+using Unity.Netcode;
 using UnityEngine;
 
-public class CameraTargetSwitcher : MonoBehaviour
+public class CameraTargetSwitcher : NetworkBehaviour
 {
     [SerializeField]
     private CinemachineCamera playerCamera;
-
-    private CinemachineBrain cinemachineBrain;
-
     private void Awake()
     {
        Initailize();
