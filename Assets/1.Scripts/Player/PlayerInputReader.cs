d40f6bcbda16b80bd7355cc85a34bf402dbbe072
SilverLight <silverlight0522@gmail.com>
2026-06-09 13:57:48 +0900

    Add PlayerControlAuthority

--- Assets/1.Scripts/Player/PlayerInputReader.cs
+++ Assets/1.Scripts/Player/PlayerInputReader.cs
@@ -4,21 +4,44 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerInputReader : MonoBehaviour
 {
-    private PlayerInput Input;
-    private InputAction MoveAction;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private bool inputEnabled = true;
 
     public Vector2 Direction { get; private set; }
     public bool HasMoveInput => Direction.sqrMagnitude > 0.01f;
 
     private void Awake()
     {
-        Input = GetComponent<PlayerInput>();
+        playerInput = GetComponent<PlayerInput>();
 
-        MoveAction = Input.actions["Move"];
+        moveAction = playerInput.actions["Move"];
     }
 
-    private void FixedUpdate()
+    public void SetInputEnabled(bool isEnabled)
     {
-        Direction = MoveAction.ReadValue<Vector2>();
+        inputEnabled = isEnabled;
+
+        if (playerInput != null)
+            playerInput.enabled = isEnabled;
+
+        if (!inputEnabled)
+            Direction = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        if (!inputEnabled)
+        {
+            Direction = Vector2.zero;
+            return;
+        }
+
+        Direction = moveAction.ReadValue<Vector2>();
+    }
+
+    private void OnDisable()
+    {
+        Direction = Vector2.zero;
     }
 }
